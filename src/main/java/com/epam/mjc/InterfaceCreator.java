e9c74d7af0e699e0f68bd56c3699dfe773bc4a3c
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-04-28 18:44:17 +0300

    Correction Stage 1 Lambda Expressions

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        return (t) -> {
+        return t -> {
             List<Integer> list = new ArrayList<>();
             for (Integer value : t) {
                 list.add(value / divider);
